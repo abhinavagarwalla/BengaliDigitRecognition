5f2b411ef243f4d856ccf158c8800c80f8cd3e1c
Abhinav Agarwalla <navabhi174@gmail.com>
2016-09-21 21:49:56 +0200

    Updating Sauvola

--- preprocessing/binary.sh
+++ preprocessing/binary.sh
@@ -1,9 +1,10 @@
 filename="$1"
-#size="$2"
-rm images_binary
-mkdir images_binary
+size="$2"
+rm ../data/images_sauvola_"$size" -r
+mkdir ../data/images_sauvola_"$size"
 while read -r line
 do
 	#convert ../data/images_resized_"$size"/"$line" -monochrome -density 300 ../data/images_binary_"$size"/"$line"
-	convert ../data/images/"$line" -monochrome -density 300 ../data/images_binary/"$line"
+	#convert ../data/images/"$line" -monochrome -density 300 ../data/images_binary/"$line"
+	./sauvola ../data/images_resized_"$size"/"$line" ../data/images_sauvola_"$size"/"$line"
 done < "$filename"
