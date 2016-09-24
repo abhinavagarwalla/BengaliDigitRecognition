acb169710cf8f3e56fac5ab5958bddf4abdb1ab7
abhinavcoder <abhinav.aajkal.kumar@gmail.com>
2016-09-24 18:58:43 +0530

    Otsu added

--- preprocessing/binary.sh
+++ preprocessing/binary.sh
@@ -1,10 +1,10 @@
 filename="$1"
 size="$2"
-rm ../data/images_sauvola_"$size" -r
-mkdir ../data/images_sauvola_"$size"
+rm ../data/images_otsu_"$size" -r
+mkdir ../data/images_otsu_"$size"
 while read -r line
 do
 	#convert ../data/images_resized_"$size"/"$line" -monochrome -density 300 ../data/images_binary_"$size"/"$line"
 	#convert ../data/images/"$line" -monochrome -density 300 ../data/images_binary/"$line"
-	./sauvola ../data/images_resized_"$size"/"$line" ../data/images_sauvola_"$size"/"$line"
+	./a.out ../data/images_resized_"$size"/"$line" ../data/images_otsu_"$size"/"$line"
 done < "$filename"
